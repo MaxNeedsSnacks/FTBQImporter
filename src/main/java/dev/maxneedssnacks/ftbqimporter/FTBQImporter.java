71ddd8d35a8eeb35b2e1f925df8e216f5cc3483d
Max <maxh2709@gmail.com>
2019-12-11 17:13:00 +0100

    Add NBT Converter (thanks Funwayguy!)
    This is going to be used in the future to import quests rather than using Gson everywhere

--- src/main/java/dev/maxneedssnacks/ftbqimporter/FTBQImporter.java
+++ src/main/java/dev/maxneedssnacks/ftbqimporter/FTBQImporter.java
@@ -6,16 +6,16 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Mod(
-        modid = "ftbqimporter",
-        name = "FTB Quests Importer",
-        version = "1.0.0",
+        modid = FTBQImporter.MOD_ID,
+        name = FTBQImporter.MOD_NAME,
+        version = FTBQImporter.VERSION,
         acceptableRemoteVersions = "*",
         dependencies = "required-after:ftblib@[0.0.0ftblib,);required-after:ftbquests",
         acceptedMinecraftVersions = "[1.12.2]"
 )
 public class FTBQImporter {
-    public static final String MOD_ID = "ftbquestimporter";
-    public static final String MOD_NAME = "FTB Quest Importer";
+    public static final String MOD_ID = "ftbqimporter";
+    public static final String MOD_NAME = "FTB Quests Importer";
     public static final String VERSION = "1.0.0";
     public static final Logger LOGGER = LogManager.getLogger("FTB Quests Importer");
 
